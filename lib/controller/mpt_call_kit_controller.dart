2c2558e06d6d1b264a72fc0bb1ff1fc3b337088a
mpt-hienhh <hienhh@mpt.com.vn>
2025-03-12 00:06:55 +0700

    change from activity-view to native-view successfully

--- lib/controller/mpt_call_kit_controller.dart
+++ lib/controller/mpt_call_kit_controller.dart
@@ -242,9 +242,9 @@ class MptCallKitController {
           Navigator.pop(context);
           onBusy?.call();
         }
-        // if (call.method == 'onCallEnded') {
-        //   Navigator.pop(context);
-        // }
+        if (call.method == 'onCallEnded' && Navigator.canPop(context)) {
+          Navigator.pop(context);
+        }
       });
 
       final bool result = await channel.invokeMethod(
