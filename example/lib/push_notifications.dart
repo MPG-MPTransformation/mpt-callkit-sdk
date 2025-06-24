b068b03a58b1d4eb70c8276242ff1a5981641f5a
mpt-hienhh <hienhh@mpt.com.vn>
2025-06-24 15:48:59 +0700

    fix local noti Initialization on Android example

--- example/lib/push_notifications.dart
+++ example/lib/push_notifications.dart
@@ -48,11 +48,11 @@ class PushNotifications {
 
       // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
       const AndroidInitializationSettings initializationSettingsAndroid =
-          AndroidInitializationSettings('@drawable/ic_notification');
+          AndroidInitializationSettings('@drawable/icon');
 
       final DarwinInitializationSettings initializationSettingsDarwin =
           DarwinInitializationSettings(
-        onDidReceiveLocalNotification: (id, title, body, payload) => null,
+        onDidReceiveLocalNotification: (id, title, body, payload) {},
       );
 
       const LinuxInitializationSettings initializationSettingsLinux =
@@ -120,7 +120,7 @@ class PushNotifications {
         importance: Importance.high,
         priority: Priority.high,
         showWhen: true,
-        icon: '@drawable/ic_notification',
+        icon: '@drawable/icon',
       );
 
       const NotificationDetails notificationDetails =
